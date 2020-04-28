98eb5d6c8877ac98531a0d4f1b49e02ae975ee9b
Muesli <noseparaquehagoesto@gmail.com>
2020-04-28 15:01:19 +0200

    Shipping Price Test

---
+++ ShippingPrice_test.py
@@ -0,0 +1,33 @@
+def ground_shipping(weight):
+  if weight > 10:
+    return (weight * 4.75) + 20.
+  elif weight > 6 and weight <= 10:
+    return (weight * 4.) + 20.
+  elif weight > 2 and weight <= 6:
+    return (weight * 3.) + 20.
+  else:
+    return (weight * 1.5) + 20.
+
+def drone_shipping(weight):
+  if weight > 10:
+    return weight * 14.25
+  elif weight > 6 and weight <= 10:
+    return weight * 12.
+  elif weight > 2 and weight <= 6:
+    return weight * 9.
+  else:
+    return weight * 4.5
+  
+def shipping_cost(weight):
+  drone = drone_shipping(weight)
+  ground = ground_shipping(weight)
+  premium = 125
+  if drone < ground and drone < premium:
+    return print("La forma mas barata es por dron, y cuesta: " + str(drone))
+  elif ground < drone and ground < premium:
+    return print("La forma mas barata es por tierra, y cuesta: " + str(ground))
+  else:
+    return print("La forma mas barata es envío premium por tierra, y cuesta: " + str(premium))
+
+shipping_cost(43)
+  
